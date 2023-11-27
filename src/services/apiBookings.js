ca32acd17e2d8a5835ab8091b7d433482850c1ff
Yurii Zaliskyi <yuriizaliskii@gmail.com>
2023-11-27 14:25:24 +0200

    apibookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,7 +1,7 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter }) {
   let query = supabase
     .from("bookings")
     .select(
