dbcb8ab8c5a529cf6fc8bfc0550eabe47d5dd539
Yurii Zaliskyi <yuriizaliskii@gmail.com>
2023-11-27 15:34:25 +0200

    add-api-side-sort

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,16 +1,20 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
       "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice, cabins(name),guests(fullName, email)"
     );
+  //FILTER
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+  //SORT
 
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
-
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
   const { data, error } = await query;
 
   if (error) {
